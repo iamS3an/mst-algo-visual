1bc266abd535e90c08b6e3305a4069cead44bd80
iamS3an <seanlin12345@gmail.com>
2025-03-18 21:11:30 +0000

    add the text update for botton when using

--- src/components/Interface.jsx
+++ src/components/Interface.jsx
@@ -69,14 +69,13 @@ function Interface() {
         { id: 'example', text: 'Example', action: handleReload, position: { bottom: '20px', right: '20px' }, colors: { default: '#007BFF', hover: '#0056b3' } },
         { id: 'clear', text: 'Clear', action: handleClearScene, position: { bottom: '20px', left: '20px' }, colors: { default: '#FFA000', hover: '#FF8000' } },
         { id: 'addNode', text: 'Add Node', action: () => toggleMode(null, 'addNode'), position: { top: '20px', left: '20px' }, colors: { default: '#02C874', hover: '#02A05D' } },
-        { id: 'removeNode', text: 'Remove Node', action: () => toggleMode('removeNode', 'removeNode'), position: { top: '20px', left: '150px' }, colors: { default: '#02C874', hover: '#02A05D' } },
-        { id: 'addEdge', text: 'Add Edge', action: () => toggleMode('addEdge', 'addEdge'), position: { top: '80px', left: '20px' }, colors: { default: '#3B3B3B', hover: '#363636' } },
-        { id: 'removeEdge', text: 'Remove Edge', action: () => toggleMode('removeEdge', 'removeEdge'), position: { top: '80px', left: '150px' }, colors: { default: '#3B3B3B', hover: '#363636' } },
-        { id: 'selectStart', text: 'Select Start Point', action: () => toggleMode('selectStart', 'selectStart'), position: { top: '140px', left: '20px' }, colors: { default: '#FF0000', hover: '#CC0000' } },
+        { id: 'removeNode', text: activeMode === 'removeNode' ? 'Cancel' : 'Remove Node', action: () => toggleMode('removeNode', 'removeNode'), position: { top: '20px', left: '150px' }, colors: { default: '#02C874', hover: '#02A05D' } },
+        { id: 'addEdge', text: activeMode === 'addEdge' ? 'Cancel' : 'Add Edge', action: () => toggleMode('addEdge', 'addEdge'), position: { top: '80px', left: '20px' }, colors: { default: '#3B3B3B', hover: '#363636' } },
+        { id: 'removeEdge', text: activeMode === 'removeEdge' ? 'Cancel' : 'Remove Edge', action: () => toggleMode('removeEdge', 'removeEdge'), position: { top: '80px', left: '150px' }, colors: { default: '#3B3B3B', hover: '#363636' } },
+        { id: 'selectStart', text: activeMode === 'selectStart' ? 'Cancel' : 'Select Start Point', action: () => toggleMode('selectStart', 'selectStart'), position: { top: '140px', left: '20px' }, colors: { default: '#FF0000', hover: '#CC0000' } },
     ];
 
     const modeMessages = {
-        addNode: 'Click anywhere to add a node',
         removeNode: 'Select a node to delete',
         addEdge: 'Select two nodes to connect',
         removeEdge: 'Select two nodes to disconnect',
