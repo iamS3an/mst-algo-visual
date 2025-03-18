4d3afa0f35bd613d48f9a603b69f319c46bac7df
iamS3an <seanlin12345@gmail.com>
2025-03-18 23:35:05 +0000

    refactor

--- src/utils/algo.js
+++ src/utils/algo.js
@@ -1,9 +1,11 @@
 export function prim(nodes, edges, algoSteps) {
     algoSteps.length = 0;
+
     const startNode = nodes.find((node) => node.userData && node.userData.start);
     if (!startNode) {
         return;
     }
+
     const visited = new Set();
     visited.add(startNode);
     algoSteps.push(startNode);
@@ -11,27 +13,20 @@ export function prim(nodes, edges, algoSteps) {
     while (visited.size < nodes.length) {
         const candidateEdges = edges.filter((edge) => {
             const { nodeA, nodeB } = edge.userData;
-            return (visited.has(nodeA) && !visited.has(nodeB)) || (visited.has(nodeB) && !visited.has(nodeA));
+            return visited.has(nodeA) !== visited.has(nodeB);
         });
 
         if (candidateEdges.length === 0) {
             break;
         }
 
-        candidateEdges.sort((a, b) => {
-            return (a.userData.weight || 0) - (b.userData.weight || 0);
-        });
-
+        candidateEdges.sort((a, b) => (a.userData.weight || 0) - (b.userData.weight || 0));
         const chosenEdge = candidateEdges[0];
         algoSteps.push(chosenEdge);
 
         const { nodeA, nodeB } = chosenEdge.userData;
-        if (visited.has(nodeA) && !visited.has(nodeB)) {
-            visited.add(nodeB);
-            algoSteps.push(nodeB);
-        } else if (visited.has(nodeB) && !visited.has(nodeA)) {
-            visited.add(nodeA);
-            algoSteps.push(nodeA);
-        }
+        const nodeToAdd = visited.has(nodeA) ? nodeB : nodeA;
+        visited.add(nodeToAdd);
+        algoSteps.push(nodeToAdd);
     }
 }
